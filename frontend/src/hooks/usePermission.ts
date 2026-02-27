ba8c0bf421bcd6fde63106cdcab5bba057d0821d
Felipe Perera <fpfelipeperera@gmail.com>
2026-02-26 22:25:38 -0300

    Backend

--- frontend/src/hooks/usePermission.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from "react";
-import { can } from "../constants/permissions";
-import { useUserStore } from "../state/user-store";
-import type { Action, Resource } from "../types/roles";
-
-export function usePermission(resource: Resource, action: Action): boolean {
-  const role = useUserStore((s) => s.user.role);
-  return useMemo(() => can(role, resource, action), [role, resource, action]);
-}
